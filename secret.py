c41558ac976e658d586c454a124b76ef43fc860e
Juneki Hong <junekihong@gmail.com>
2015-12-05 04:34:51 -0500

    pushing an anonymized secret.py file

---
+++ secret.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#article search api
+#search = "YOUR KEY HERE"
+
+#most popular api
+#popular = "YOUR KEY HERE"
+
+#Times Newswire API
+#newswire = "YOUR KEY HERE"
+
+#top stories API
+topstories = "YOUR KEY HERE"
